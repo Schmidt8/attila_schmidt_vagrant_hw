2c4314b324575109cc51f1005f6182a6a10c7711
Schmidt8 <92981332+Schmidt8@users.noreply.github.com>
2023-12-20 22:01:05 +0100

    set def instance nama

--- my-module/variables.tf
+++ my-module/variables.tf
@@ -1,7 +1,7 @@
 variable "instance_name" {
 description = "EC2 instance neve"
 type = string
-default = "EC2_valami2"
+default = "Attila_Schmidt"
 }
 variable "instance_type" {
 description = "EC2 instance type"
